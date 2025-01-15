f70e009378d57425fd8b26e85c2cffa2b6103fd7
cwang12197 <wchristina4@gmail.com>
2025-01-15 16:13:42 -0500

    update schedule

--- deaf_ui/src/Schedule/Schedule.js
+++ deaf_ui/src/Schedule/Schedule.js
@@ -5,12 +5,12 @@ const Schedule = () => {
     const schedule = [
         { time: "9:30", event: "Doors Open with Coffee!" },
         { time: "10:00-10:45", event: "Erin Moriarity" },
-        { time: "11:15-12:15", event: "Terry Galloway Performance" },
-        { time: "12:30-1:00", event: "Lunch" },
+        { time: "11:15-12:15", event: "Terry Galloway" },
+        { time: "12:30-1:15", event: "Lunch (provided) - New Dominion will sell books" },
         { time: "1:15-2:00", event: "Christopher Krentz" },
         { time: "2:30-3:30", event: "Kristen Harmon" },
         { time: "4:00-5:00", event: "Leah Hager Cohen" },
-        { time: "5:30-6:00", event: "Dinner" },
+        { time: "5:30-6:00", event: "Dinner (provided) - New Dominion will sell books" },
         { time: "6:00-7:00", event: "Carol Padden" },
         { time: "7:30-9:30", event: "Open Mic Storytelling Event" },
     ];
