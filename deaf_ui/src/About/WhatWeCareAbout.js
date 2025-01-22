229fc0e7617b73c3dc9dd413b8f43c2760fd4052
cwang12197 <wchristina4@gmail.com>
2025-01-21 20:29:42 -0500

    add location info to about

--- deaf_ui/src/About/WhatWeCareAbout.js
+++ deaf_ui/src/About/WhatWeCareAbout.js
@@ -10,11 +10,13 @@ const WhatWeCareAbout = () => {
                     <div className="about-text-section">
                         <p className="description">
                             The Deaf Literature Festival is a festival that was created to bring together all people in a large celebration of Deaf literature. With authors from Canada to San Francisco to our very own Charlottesville, our authors represent a wide range of literature and experiences. Take a look at our recommended reading list to see what books our authors have written as well as a diverse list of books you may also enjoy by Deaf and CODA authors. There will be food, drinks, and coffee provided. Parking is free on Saturdays and in depth parking maps will be provided as the event nears. We hope to see you at the festival!
-
                         </p>
                         <p className='description-event'>
                             <b>This is a free event! No registration needed!</b>
                         </p>
+                        <p className="description">Location is on UVA Grounds, room TBA. Building and parking options will be posted as soon as we know more.
+                        </p>
+
                     </div>
                 </div>
             </div>
